028a2b3a7f0ed756788400bc0066ff2077704879
Arunesh.N <125756336+AruneshN@users.noreply.github.com>
2026-03-15 17:22:01 +0530

    Update render_template.py

--- Templates_jinja/render_template.py
+++ Templates_jinja/render_template.py
@@ -24,11 +24,20 @@ app=Flask(__name__,template_folder="templates") #connect manually
 def login():
     return render_template("login.html")
 
-#2️⃣ Passing Data to Html
+#2️⃣ Passing Data to Html & 3️⃣ Multiple variables
 @app.route("/user")
 def user():
     name="arunesh"
-    return render_template("user.html",username=name)
+    age=25
+    return render_template("user.html",username=name,age=age)
+
+
+#4️⃣ Rendering Inside loop
+@app.route("/user_list")
+def usrlst():
+    lst=["arunesh","hari","john","sai","kristy","najumudhin"]
+    return render_template("user_list.html",Users=lst)
+
 
 #5️⃣ Passing Data in dictionary variables
 @app.route("/user_details")
@@ -39,6 +48,32 @@ def details():
         "id":406,
     }
     return render_template("user.html",User=user)
+
+#6️⃣ control statements
+@app.route('/control_statement')
+def control_stat():
+    lst={
+        "arunesh":{
+            "Age":22,
+            "id":1066
+        },
+        "hari":{
+            "Age":24,
+            "id":5892
+        },
+        "Sai":{
+            "Age":24,
+            "id":5855
+        },
+        "kristy":{
+            "Age":24,
+            "id":54545
+        }
+    }
+    name=["arunesh","hari","john","sai","kristy","najumudhin"]
+
+    return render_template("control_statement.html",user_lst=lst,name=name)
+
 
 
 if __name__ == "__main__":
